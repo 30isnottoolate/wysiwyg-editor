a167228c89094bbdd4d8417f3ecd9e170b5efffc
30isnottoolate <akos.varga.92@gmail.com>
2022-12-20 17:24:23 +0100

    Add basic elements and an onSelect handler

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const App: React.FC = () => {
+    const [content, setContent] = useState(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`);
+
+    const editorRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!editorRef.current) return;
+        else editorRef.current.innerHTML = content;
+    }, []);
+
+    const handleSelection = () => {
+        let sel = document.getSelection();
+        console.log("selection: " + sel);
+    }
+
     return (
-        <div>
-            So it begins...
-        </div>
+        <>
+            <button>RED</button>
+            <button>GREEN</button>
+            <button>BLUE</button>
+            <div 
+                ref={editorRef}
+                contentEditable="true" 
+                style={{ whiteSpace: "pre-wrap" }} 
+                onInput={(event) => setContent(event.currentTarget.innerHTML)} 
+                onSelect={handleSelection} />
+        </>
     )
 }
 
