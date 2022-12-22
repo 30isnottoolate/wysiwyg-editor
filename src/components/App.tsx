f386fef3934c9f1492611607dd9706ec73f46292
30isnottoolate <akos.varga.92@gmail.com>
2022-12-22 18:55:27 +0100

    Add bold button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,9 +5,12 @@ import 'draft-js/dist/Draft.css';
 const App: React.FC = () => {
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-    console.log(editorState);
-
-    return <Editor editorState={editorState} onChange={setEditorState} />;
+    return (
+        <>
+            <button onClick={() => {}}><b>B</b></button>
+            <Editor editorState={editorState} onChange={setEditorState} />
+        </>
+    );
 }
 
 export default App;
