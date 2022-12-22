00f8603f53b2fe9cc01afefa1b63cca4b09889ea
30isnottoolate <akos.varga.92@gmail.com>
2022-12-22 14:36:21 +0100

    Install and initialize draft-js

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 const App: React.FC = () => {
-    const [content, setContent] = useState(`Lorem ipsum dolor sit amet, <i>consectetur</i> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`);
-    const [selectedContent, setSelectedContent] = useState("");
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-    const editorRef = useRef<HTMLDivElement>(null);
+    console.log(editorState);
 
-    useEffect(() => {
-        if (!editorRef.current) return;
-        else editorRef.current.innerHTML = content;
-    }, []);
-
-    const handleSelection = () => {
-        let selectionData = document.getSelection();
-
-        if (selectionData?.rangeCount) {
-            let container = document.createElement("div");
-
-            for (let i = 0; i < selectionData.rangeCount; ++i) {
-                container.appendChild(selectionData.getRangeAt(i).cloneContents());
-            }
-
-            setSelectedContent(container.innerHTML);
-        }
-    }
-
-    return (
-        <>
-            <button>RED</button>
-            <button>GREEN</button>
-            <button>BLUE</button>
-            <div
-                ref={editorRef}
-                contentEditable="true"
-                style={{ whiteSpace: "pre-wrap" }}
-                onInput={(event) => setContent(event.currentTarget.innerHTML)}
-                onSelect={handleSelection} />
-        </>
-    )
+    return <Editor editorState={editorState} onChange={setEditorState} />;
 }
 
 export default App;
