16d872e5827b37ea03588fc6f42aee15eba24024
30isnottoolate <akos.varga.92@gmail.com>
2022-12-27 14:51:55 +0100

    Revert back to initial state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
-import { Editor, EditorState } from 'draft-js';
-import 'draft-js/dist/Draft.css';
-import './App.css';
+import React from "react";
 
 const App: React.FC = () => {
-    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-
     return (
-        <>
-            <button onClick={() => {}}><b>B</b></button>
-            <button onClick={() => {}}><i>I</i></button>
-            <button onClick={() => {}}><u>U</u></button>
-            <Editor editorState={editorState} onChange={setEditorState} />
-        </>
-    );
+        <div>
+            So it begins...
+        </div>
+    )
 }
 
 export default App;
