a5fae6b13dc4ab34acd7ba644050d390899bbc8e
30isnottoolate <akos.varga.92@gmail.com>
2022-12-27 15:29:37 +0100

    Add removeFormat and implement

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -28,11 +28,25 @@ const App: React.FC = () => {
         }
     }
 
+    const removeFormat = () => {
+        const selection = window.getSelection();
+        if (selection && selection.rangeCount) {
+            let range = selection.getRangeAt(0);
+            let selectedTextNode = document.createTextNode(range.toString());
+            range.deleteContents();
+            range.insertNode(selectedTextNode);
+            range.selectNode(selectedTextNode);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
     return (
         <div>
             <button onClick={() => applyFormat("b")}><b>B</b></button>
             <button onClick={() => applyFormat("i")}><i>I</i></button>
             <button onClick={() => applyFormat("u")}><u>U</u></button>
+            <button onClick={() => removeFormat()}>X</button>
             <button onClick={() => applyColor()} style={{ backgroundColor: fontColor }}>C</button>
             <input type="color" onChange={(event) => setFontColor(event.currentTarget.value)} />
             <div id="editor" contentEditable={true} />
