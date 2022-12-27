49f9291f7caf9126e93133d1180ae6568dff854f
30isnottoolate <akos.varga.92@gmail.com>
2022-12-27 15:03:33 +0100

    Add basic elements

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const App: React.FC = () => {
     return (
         <div>
-            So it begins...
+            <button><b>B</b></button>
+            <button><i>I</i></button>
+            <button><u>U</u></button>
+            <button style={{backgroundColor: "#ff0000"}}>C</button>
+            <div id="editor" contentEditable={true} placeholder="Write something..." />
         </div>
     )
 }
