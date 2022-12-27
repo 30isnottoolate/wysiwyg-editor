06549c849019247e19bd7d2ff9bc65e5eda0a005
30isnottoolate <akos.varga.92@gmail.com>
2022-12-27 15:09:14 +0100

    Add toggleFormat function to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 const App: React.FC = () => {
+    const toggleFormat = (format: string) => {
+        const selection = window.getSelection();
+
+        if (selection && selection.rangeCount) {
+            const range = selection.getRangeAt(0);
+            const newNode = document.createElement(format);
+            newNode.appendChild(range.extractContents());
+            range.insertNode(newNode);
+        }
+    };
+
     return (
         <div>
-            <button><b>B</b></button>
-            <button><i>I</i></button>
-            <button><u>U</u></button>
-            <button style={{backgroundColor: "#ff0000"}}>C</button>
-            <div id="editor" contentEditable={true} placeholder="Write something..." />
+            <button onClick={() => toggleFormat("b")}><b>B</b></button>
+            <button onClick={() => toggleFormat("i")}><i>I</i></button>
+            <button onClick={() => toggleFormat("u")}><u>U</u></button>
+            <button style={{ backgroundColor: "#ff0000" }}>C</button>
+            <div id="editor" contentEditable={true} />
         </div>
     )
 }
