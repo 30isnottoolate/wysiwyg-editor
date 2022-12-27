e144cbd51ab8d6d2d99e536f3a4ede7f77780aa5
30isnottoolate <akos.varga.92@gmail.com>
2022-12-27 17:39:37 +0100

    Add insertEnter and removeColor functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,19 +12,9 @@ const App: React.FC = () => {
 
             formatNode.appendChild(range.extractContents());
             range.insertNode(formatNode);
-        }
-    };
-
-    const applyColor = () => {
-        const selection = window.getSelection();
-
-        if (selection && selection.rangeCount) {
-            const range = selection.getRangeAt(0);
-            const colorNode = document.createElement("span");
-
-            colorNode.style.color = fontColor;
-            colorNode.appendChild(range.extractContents());
-            range.insertNode(colorNode);
+            range.selectNode(formatNode);
+            selection.removeAllRanges();
+            selection.addRange(range);
         }
     }
 
@@ -43,6 +33,51 @@ const App: React.FC = () => {
         }
     }
 
+    const insertEnter = () => {
+        const selection = window.getSelection();
+
+        if (selection && selection.rangeCount) {
+            const range = selection.getRangeAt(0);
+            const selectedTextNode = document.createTextNode(`\n`);
+
+            range.deleteContents();
+            range.insertNode(selectedTextNode);
+            range.selectNode(selectedTextNode);
+
+            range.collapse(true);
+
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
+    const removeColor = () => {
+        let sel = document.getSelection();
+        let candidates = document?.querySelectorAll("span");
+
+        candidates?.forEach((candidate) => {
+            if (sel?.containsNode(candidate, true)) {
+                candidate.replaceWith(document.createTextNode(candidate.innerHTML));
+            }
+        });
+    }
+
+    const applyColor = () => {
+        const selection = window.getSelection();
+
+        if (selection && selection.rangeCount) {
+            const range = selection.getRangeAt(0);
+            const colorNode = document.createElement("span");
+
+            colorNode.style.color = fontColor;
+            colorNode.appendChild(range.extractContents());
+            range.insertNode(colorNode);
+            range.selectNode(colorNode);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
     return (
         <div>
             <button onClick={() => applyFormat("b")}><b>B</b></button>
@@ -51,7 +86,9 @@ const App: React.FC = () => {
             <button onClick={() => removeFormat()}>X</button>
             <button onClick={() => applyColor()} style={{ backgroundColor: fontColor }}>C</button>
             <input type="color" onChange={(event) => setFontColor(event.currentTarget.value)} />
-            <div id="editor" contentEditable={true} >Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <button onClick={() => removeColor()}>rem</button>
+            <button onClick={() => insertEnter()}>enter</button>
+            <div id="editor" contentEditable={true} style={{ whiteSpace: "pre-wrap" }} >Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                 nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
                 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
