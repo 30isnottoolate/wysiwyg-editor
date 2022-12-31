224f08bd15658b2ff6233a8cecf33acceaeffacd
30isnottoolate <akos.varga.92@gmail.com>
2022-12-31 18:30:42 +0100

    Add handleInput to fix new line issue

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -99,6 +99,12 @@ const App: React.FC = () => {
         }
     }
 
+    const handleInput = (event: React.FormEvent<HTMLDivElement>) => {
+        if (event.currentTarget.lastChild && event.currentTarget.lastChild.nodeName !== "BR") {
+            event.currentTarget.append(document.createElement("br"));
+        }
+    }
+
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -180,13 +186,14 @@ const App: React.FC = () => {
                 spellCheck={false}
                 contentEditable={true}
                 suppressContentEditableWarning={true}
-                onKeyDown={event => handleKeyDown(event)} >
+                onKeyDown={event => handleKeyDown(event)}
+                onInput={event => handleInput(event)} >
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                 nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
                 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
                 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br/>
+                <br />
             </div>
         </div>
     )
