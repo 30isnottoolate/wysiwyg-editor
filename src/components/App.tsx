f132c455e5350461e62a90f3f1299a83260561d9
30isnottoolate <akos.varga.92@gmail.com>
2023-01-02 16:05:28 +0100

    Put editor into container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -126,7 +126,7 @@ const App: React.FC = () => {
     }
 
     return (
-        <div>
+        <>
             <div className="toolbar">
                 <button
                     className="tool"
@@ -197,21 +197,23 @@ const App: React.FC = () => {
                     X
                 </button>
             </div>
-            <div
-                id="editor"
-                spellCheck={false}
-                contentEditable={true}
-                suppressContentEditableWarning={true}
-                onKeyDown={event => handleKeyDown(event)}
-                onInput={event => handleInput(event)} >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br />
+            <div id="editor-container">
+                <div
+                    id="editor"
+                    spellCheck={false}
+                    contentEditable={true}
+                    suppressContentEditableWarning={true}
+                    onKeyDown={event => handleKeyDown(event)}
+                    onInput={event => handleInput(event)} >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
