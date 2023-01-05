b0b484a52e7238de02926b105a015487fe702587
30isnottoolate <akos.varga.92@gmail.com>
2023-01-05 15:41:52 +0100

    Remove unnecessary lines

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import Toolbar from "./Toolbar";
-import ColorPicker from "./ColorPicker";
 
 const App: React.FC = () => {
-    const [colorPickerActive, setColorPickerActive] = useState(false);
     const [fontColor, setFontColor] = useState("#ff0000");
 
     const applyFormatting = (style: string) => {
@@ -138,81 +136,8 @@ const App: React.FC = () => {
                 removeAllFormatting={removeAllFormatting}
                 applyFontColor={applyFontColor}
                 fontColor={fontColor}
-                setFontColor={setFontColor} />
-            {/* <div className="toolbar">
-                <button
-                    className="tool"
-                    title="Bold"
-                    onClick={() => applyFormatting("b")}>
-                    <b>B</b>
-                </button>
-                <button
-                    className="tool italic"
-                    title="Italic"
-                    onClick={() => applyFormatting("i")}>
-                    <i>I</i>
-                </button>
-                <button
-                    className="tool"
-                    title="Underline"
-                    onClick={() => applyFormatting("u")}>
-                    <u>U</u>
-                </button>
-                <button
-                    className="tool"
-                    title="Strikethrough"
-                    onClick={() => applyFormatting("s")}>
-                    <s>S</s>
-                </button>
-                <button
-                    className="tool"
-                    title="Superscript"
-                    onClick={() => applyFormatting("sup")}>
-                    A<sup>2</sup>
-                </button>
-                <button
-                    className="tool"
-                    title="Subscript"
-                    onClick={() => applyFormatting("sub")}>
-                    A<sub>2</sub>
-                </button>
-                <button
-                    className="tool"
-                    title="Remove All Formating"
-                    onClick={() => removeAllFormatting()}>
-                    X
-                </button>
-                <div
-                    className="color-tool" >
-                    <button
-                        className="font-color"
-                        title="Font Color"
-                        onClick={() => applyFontColor()}
-                        style={{ color: fontColor }}>
-                        A
-                    </button>
-                    <button
-                        className="tool-expander"
-                        onClick={() => setColorPickerActive(prevState => !prevState)} >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="8"
-                            height="8"
-                            viewBox="-1 0 18 12" >
-                            <polygon
-                                points="0,0 8,10 16,0 8,10 0,0"
-                                style={{ fill: "none", stroke: "#000000", strokeWidth: "3px" }}
-                            />
-                        </svg>
-                    </button>
-                    {colorPickerActive &&
-                        <ColorPicker
-                            setFontColor={setFontColor}
-                            setColorPickerActive={setColorPickerActive}
-                        />
-                    }
-                </div>
-            </div> */}
+                setFontColor={setFontColor} 
+            />
             <div id="editor-container">
                 <div
                     id="editor"
